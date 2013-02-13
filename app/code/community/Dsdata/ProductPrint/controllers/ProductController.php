d271695ea3346495cba2aaa7f4699f265030f338
Daniel Sasse <info@golox-web.de>
2013-02-13 23:37:07 +0100

    Aktualisierung - Autorendaten

--- app/code/community/Dsdata/ProductPrint/controllers/ProductController.php
+++ app/code/community/Dsdata/ProductPrint/controllers/ProductController.php
@@ -1,16 +1,13 @@
 <?php
-
 /**
  * @category Dsdata
  * @package Dsdata_ProductPrint
- * @authors Daniel Sasse <info@golox-web.de, http://www.golox-web.de/>
- * @developer Daniel Sasse <info@golox-web.de, http://www.golox-web.de/>  
- * @version 0.1.0
- * @copyright Golox-Web.de, Daniel Sasse
+ * @authors Daniel Sasse <info@dsdata.de, http://www.dsdata.de/>
+ * @developer Daniel Sasse <info@dsdata.de, http://www.dsdata.de/>  
+ * @version 0.1.1
+ * @copyright dsdata.de, Daniel Sasse
  * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)  
  */
-
-
 class Dsdata_ProductPrint_ProductController extends Mage_Core_Controller_Front_Action
 {
     //protected $_designProductSettingsApplied = array();
